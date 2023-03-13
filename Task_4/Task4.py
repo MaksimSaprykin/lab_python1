adf96e3544736fe7921564ff1bcc44bfd12b8363
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-14 00:55:30 +0200

    Baseline data for implementation entered task 4

---
+++ Task_4/Task4.py
@@ -0,0 +1,28 @@
+# Multi paradigm programming language
+# Task 4
+# Saprykin Maksym
+# zachet_number = 16
+
+from math import *
+
+course_name = '\'Multi paradigm programming language\''
+task_number = 'Task 4'
+first_name_student = 'Maksym'
+last_name = 'Saprykin'
+zachet_number = '16'
+formula = 'sin(x) * x'
+step_x = 0.001
+
+print(course_name + ': ' + task_number)
+print(first_name_student + ' ' + last_name + ', ' + zachet_number, '\n')
+
+# ishodna formula
+def func ():
+    f = eval(formula)
+    return f
+
+# Description Tasks
+print('1. Example:')
+print('y = ', formula)
+
+# input data
