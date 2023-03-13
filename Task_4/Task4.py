459e613eb3fae9df7fbd87726c165fefe4fc3bc7
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-14 01:34:04 +0200

    Task 4 is completed

--- Task_4/Task4.py
+++ Task_4/Task4.py
@@ -10,6 +10,7 @@ task_number = 'Task 4'
 first_name_student = 'Maksym'
 last_name = 'Saprykin'
 zachet_number = '16'
+
 formula = 'sin(x) * x'
 step_x = 0.001
 
@@ -26,3 +27,26 @@ print('1. Example:')
 print('y = ', formula)
 
 # input data
+
+a = float(input('enter the beginning of the segment  [a, b], a ='))
+b = float(input('enter the end of the segment   [a, b], B ='))
+
+# segment_a_b = []
+
+def f_range (start, stop, step): # range() на пряму не працює з float, то обходимо наступним чином,
+        i = start
+        while i < stop:
+            yield i             # зробимо генератор
+            i += step
+
+for i in f_range(a, b+step_x, step_x):
+    x = i
+    if i == a :
+        head_min_func = func()
+    min_func = func()
+    if head_min_func > min_func :
+        head_min_func = min_func
+
+    print(i, min_func, head_min_func)
+
+print('На відрізку [', a, ', ', b, ']', 'функція ', formula, 'приймає min значення', head_min_func)
