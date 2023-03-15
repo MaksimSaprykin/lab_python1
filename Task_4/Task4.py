de4f0ee0982349ba05b7660a4173ced929ad65b0
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-16 00:55:42 +0200

    header corrected

--- Task_4/Task4.py
+++ Task_4/Task4.py
@@ -3,23 +3,25 @@
 # Saprykin Maksym
 # zachet_number = 16
 
-from math import *
+from math import *  # Знайти рішення як амінити на 'import math as m'
 
-course_name = '\'Multi paradigm programming language\''
-task_number = 'Task 4'
-first_name_student = 'Maksym'
-last_name = 'Saprykin'
-zachet_number = '16'
+# но тоді доведеться перед мат.функ. дописувати m.sin
 
+COURSE_NAME = '\'Multi paradigm programming language\''
+TASK_NUMBER = 'Task 4'
+STUDENT = {'first_name':'Maksym', 'last_name': 'Saprykin', 'zachet_number':'№16'}
+STUDENT = {'Maksym', 'Saprykin', 'zachet_number - 16'}
 FORMULA = 'sin(x) * x'
 step_x = 0.001
 
-print(course_name + ': ' + task_number)
-print(first_name_student + ' ' + last_name + ', ' + zachet_number, '\n')
+print(f'{COURSE_NAME}:{TASK_NUMBER}')
+print(STUDENT,'\n')
+
 
 # ishodna formula
-def func(p):
-    return eval(f'sin({p})*{p}')
+def calculation_expression(x):
+    return eval(f'sin({x})*{x}')
+
 
 # Description Tasks
 print('1. Example:')
@@ -31,21 +33,13 @@ a = float(input('enter the beginning of the segment  [a, b], a ='))
 b = float(input('enter the end of the segment   [a, b], B ='))
 
 
-def f_range (start, stop, step): # range() на пряму не працює з float, то обходимо наступним чином,
-        i = start
-        while i < stop:
-            yield i             # зробимо генератор
-            i += step
+def f_range(start, stop, step):  # range() на пряму не працює з float, то обходимо наступним чином,
+    i = start
+    while i < stop:
+        yield i  # зробимо генератор
+        i += step
 
 
-min_func = min([func(x) for x in f_range(a, b+step_x, step_x)])
-
-#for i in f_range(a, b+step_x, step_x):
-#    x = i
-#    if i == a :
-#        head_min_func = func()
-#    min_func = func()
-#    if head_min_func > min_func :
-#        head_min_func = min_func
+min_func = min([calculation_expression(x) for x in f_range(a, b + step_x, step_x)])
 
 print('На відрізку [', a, ', ', b, ']', 'функція ', FORMULA, 'приймає min значення', min_func)
