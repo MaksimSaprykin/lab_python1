2641e8c7b6c61b8cd7f01f58e2cda879be64c5f9
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-16 00:13:44 +0200

    Created list for min_func

--- Task_4/Task4.py
+++ Task_4/Task4.py
@@ -11,27 +11,25 @@ first_name_student = 'Maksym'
 last_name = 'Saprykin'
 zachet_number = '16'
 
-formula = 'sin(x) * x'
+FORMULA = 'sin(x) * x'
 step_x = 0.001
 
 print(course_name + ': ' + task_number)
 print(first_name_student + ' ' + last_name + ', ' + zachet_number, '\n')
 
 # ishodna formula
-def func ():
-    f = eval(formula)
-    return f
+def func(p):
+    return eval(f'sin({p})*{p}')
 
 # Description Tasks
 print('1. Example:')
-print('y = ', formula)
+print('y = ', FORMULA)
 
 # input data
 
 a = float(input('enter the beginning of the segment  [a, b], a ='))
 b = float(input('enter the end of the segment   [a, b], B ='))
 
-# segment_a_b = []
 
 def f_range (start, stop, step): # range() на пряму не працює з float, то обходимо наступним чином,
         i = start
@@ -39,12 +37,15 @@ def f_range (start, stop, step): # range() на пряму не працює з 
             yield i             # зробимо генератор
             i += step
 
-for i in f_range(a, b+step_x, step_x):
-    x = i
-    if i == a :
-        head_min_func = func()
-    min_func = func()
-    if head_min_func > min_func :
-        head_min_func = min_func
 
-print('На відрізку [', a, ', ', b, ']', 'функція ', formula, 'приймає min значення', head_min_func)
+min_func = min([func(x) for x in f_range(a, b+step_x, step_x)])
+
+#for i in f_range(a, b+step_x, step_x):
+#    x = i
+#    if i == a :
+#        head_min_func = func()
+#    min_func = func()
+#    if head_min_func > min_func :
+#        head_min_func = min_func
+
+print('На відрізку [', a, ', ', b, ']', 'функція ', FORMULA, 'приймає min значення', min_func)
