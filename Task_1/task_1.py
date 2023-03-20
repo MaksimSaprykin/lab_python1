7294b3b55554d1342ae0d06ff84a19f22489fec7
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-20 23:05:58 +0200

    made on functions

--- Task_1/task_1.py
+++ Task_1/task_1.py
@@ -12,16 +12,22 @@ STUDENT = {'Maksym', 'Saprykin', 'zachet_number - 16'}
 first_name_teacher = 'Vladislav'
 export_formula = '7.8-(1+2.1/3)/(78+21.3)'
 
+
 def print_info():
     print(f'{COURSE_NAME}:{TASK_NUMBER}')
     print(', '.join(STUDENT), '\n')
 
-def
+
+def try_python_essentials():
     str = [first_name_teacher for _ in range(45)]
     print(','.join(str))
 
-def
-    print(export_formula, '= ', eval(export_formula))
+
+def func(formula):
+    return eval(formula)
+
 
 if __name__ == '__main__':
-
+    print_info()
+    try_python_essentials()
+    print(export_formula, '= ', func(export_formula))
