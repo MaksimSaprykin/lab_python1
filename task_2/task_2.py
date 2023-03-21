4432918df5c9ea9e5eb0ebb4fe347ad3f92bb62d
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-22 01:41:50 +0200

    rename x, y, z to var_x, var_y, var_z in calculation_expression

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -41,8 +41,8 @@ def inp_data():
             break
 
 
-def calculation_expression(x1, y, z):
-    return x1 - (x1 + y / z) / (78 + y)
+def calculation_expression(var_x, var_y, var_z):
+    return var_x - (var_x + var_y / var_z) / (78 + var_y)
 
 
 def print_rezult(rezultat):
