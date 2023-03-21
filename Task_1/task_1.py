67a73affe3184921ee7b390dec960f1f2ed811c7
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-21 20:39:47 +0200

    verified by prospector

--- Task_1/task_1.py
+++ Task_1/task_1.py
@@ -9,8 +9,8 @@
 COURSE_NAME = '\'Multi paradigm programming language\''
 TASK_NUMBER = 'Task 4'
 STUDENT = {'Maksym', 'Saprykin', 'zachet_number - 16'}
-first_name_teacher = 'Vladislav'
-export_formula = '7.8-(1+2.1/3)/(78+21.3)'
+FIRST_NAME_TEACHER = 'Vladislav'
+FORMULA = '7.8-(1+2.1/3)/(78+21.3)'
 
 
 def print_info():
@@ -19,15 +19,15 @@ def print_info():
 
 
 def try_python_essentials():
-    str = [first_name_teacher for _ in range(45)]
-    print(','.join(str))
+    str45 = [FIRST_NAME_TEACHER for _ in range(45)]
+    print(','.join(str45))
 
 
-def func(formula):
-    return eval(formula)
+def f_expression():
+    print(f'{FORMULA}={7.8 - (1 + 2.1 / 3) / (78 + 21.3)}')
 
 
 if __name__ == '__main__':
     print_info()
     try_python_essentials()
-    print(export_formula, '= ', func(export_formula))
+    f_expression()
