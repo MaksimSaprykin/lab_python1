e6f4a6cc90bb6c929a9b3408e3384a14b425a9cf
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-22 02:11:51 +0200

    fixes to code po prospecto

--- Task_3/task_3.py
+++ Task_3/task_3.py
@@ -1,36 +1,57 @@
-# Multi paradigm programming language
-# Task 3
-# Saprykin Maksym
-# zachet_number = 16
-
-from math import *
+""" Multi paradigm programming language
+    Task 3
+    Saprykin Maksym
+    zachet_number = 16
+"""
+from string import Template
+import math as m
 
 COURSE_NAME = '\'Multi paradigm programming language\''
 TASK_NUMBER = 'Task 3'
 STUDENT = {'Maksym', 'Saprykin', 'zachet_number - 16'}
+t_input = Template('input ')
 
-FORMULA = '(x + 2 * x + 3) / (z - 2) + atan(z)'
 
-print(f'{COURSE_NAME}:{TASK_NUMBER}')
-print(STUDENT, '\n')
+def print_info():
+    print(f'{COURSE_NAME}:{TASK_NUMBER}')
+    print(STUDENT, '\n')
+
+
+# Description Tasks
+def description_task(formula):
+    print('1. Solve an example:')
+    print(formula, '= ?')
 
 
 # ishodna formula
-def calculation_expression(x, z):
-    return eval(f'({x}+2*{x}+3)/({z}-2)+atan({z})')
+def calculation_expression(var_x, var_z):
+    return (var_x + 2 * var_x + 3) / (var_z - 2) + m.atan(var_z)
 
-# Description Tasks
-print('1. Example:')
-print('y = ', FORMULA)
 
 # input data
+def inp_data():
+    while True:
+        try:
+            x_i = float(input(f'{t_input.template}: x = '))
+            z_i = float(input(f'{t_input.template}: z = '))
+        except ValueError:
+            print('Enter not number')
+            continue
+        else:
+            try:
+                print_rezult(calculation_expression(x_i, z_i))
+            except ZeroDivisionError:
+                print('Input incorrect, divide by 0 cannot be')
+                continue
+            break
 
-while True:
-    z = float(input('input z:'))
-    if z != 2:
-        break
-    print('Not input z = 2, divide by 0 cannot be')
 
-x = float(input('input x:'))
+def print_rezult(rezultat):
+    print(F, '=', rezultat)
 
-print('y = ', FORMULA, '=', calculation_expression(x, z))
+
+if __name__ == '__main__':
+    F = '(x + 2 * x + 3) / (z - 2) + ctag(z)'
+    print_info()
+    description_task(F)
+    inp_data()
