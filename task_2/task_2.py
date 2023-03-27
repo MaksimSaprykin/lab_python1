e1bcca66d06b4f47ec9cabba2dc6c752ae600348
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-27 11:12:37 +0300

    task 2/1

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -10,9 +10,9 @@ TEMPLATE = 'Input {}'
 
 
 # Description Tasks
-def description_task(formula):
-    print('1. Solve an example:')
-    print(formula, '= ?')
+#def description_task(formula):
+#    print('1. Solve an example:')
+#    print(formula, '= ?')
 
 
 def inp_validate(values):
@@ -28,24 +28,32 @@ def inp_data():
     x_i = inp_validate('x')
     y_i = inp_validate('y')
     z_i = inp_validate('z')
-    while True:
-        try:
-            return print_rezult(calculation_expression(x_i, y_i, z_i))
-        except ZeroDivisionError:
-            print('Input incorrect, divide by 0 cannot be')
+    while y_i == -78 or z_i == y_i:
+        if y_i == -78:
+            print('Input incorrect, y_i NOT = -78')
+            y_i = inp_validate('y')
+        if z_i == y_i:
+            print('Input incorrect, z != y')
             z_i = inp_validate('z')
+    return ut.print_rezult(calculation_expression(x_i, y_i, z_i))
+#    while True:
+#        try:
+#            return print_rezult(calculation_expression(x_i, y_i, z_i))
+#        except ZeroDivisionError:
+#            print('Input incorrect, divide by 0 cannot be')
+#            z_i = inp_validate('z')
 
 
 def calculation_expression(var_x, var_y, var_z):
     return var_x - (var_x + var_y / var_z) / (78 + var_y)
 
 
-def print_rezult(rezultat):
-    print(F, '=', rezultat)
+#def print_rezult(rezultat):
+#    print(F, '=', rezultat)
 
 
 if __name__ == '__main__':
     F = 'x - (x + y/z)/(78 + y)'
     ut.print_info(TASK_NUMBER)
-    description_task(F)
+    ut.description_task(F)
     inp_data()
