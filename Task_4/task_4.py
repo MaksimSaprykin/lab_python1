5338aadabb7179d70585b51c8cf9196fa9a691c3
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-04-05 12:06:23 +0300

    Use NumPy’s arange() functions to use decimal numbers in a start, stop and step argument to produce a range of floating-point numbers.

--- Task_4/task_4.py
+++ Task_4/task_4.py
@@ -5,24 +5,13 @@
 
 import math as m
 import utils as ut
+import numpy as np
 
 TASK_NUMBER = 'Task 4'
 FORMULA = 'sin(x) * x'
 STEP_X = 0.001
 TEMPLATE_A = 'enter the beginning of the segment  [a, b], a ='
 TEMPLATE_B = 'enter the end of the segment   [a, b], B ='
-
-
-# ishodna formula
-def calculation_expression(x):
-    return m.sin(x) * x
-
-
-# input data
-def inp_data():
-    a = ut.inp_variable('a', TEMPLATE_A)
-    b = ut.inp_variable('b', TEMPLATE_B)
-    return a, b
 
 
 def f_range(start, stop, step):  # range() на пряму не працює з float, то обходимо наступним чином,
@@ -33,11 +22,18 @@ def f_range(start, stop, step):  # range() на пряму не працює з 
 
 
 def min_func_expression(a, b):
-    return min([calculation_expression(x) for x in f_range(a, b + STEP_X, STEP_X)])
+    temp_b = a
+    if a > b:
+        b = a
+        a = temp_b
+    return min([m.sin(x) * x for x in np.arange(a, b + STEP_X, STEP_X)])
 
 
 if __name__ == '__main__':
     ut.print_info(TASK_NUMBER)
     ut.description_task(FORMULA)
-    a_b = inp_data()
-    print(f'On the segment {a_b}, function {FORMULA} takes the min value = {min_func_expression(a_b[0], a_b[1])}')
+
+    a = ut.inp_variable('a', TEMPLATE_A)
+    b = ut.inp_variable('b', TEMPLATE_B)
+
+    print(f'On the segment [{a}:{b}], function {FORMULA} takes the min value = {min_func_expression(a, b)}')
