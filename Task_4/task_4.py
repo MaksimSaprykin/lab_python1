ca359c4684d6cf84503ef186e727decb54525694
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-04-05 13:02:19 +0300

    corrected comments from prosector

--- Task_4/task_4.py
+++ Task_4/task_4.py
@@ -4,14 +4,16 @@
 # zachet_number = 16
 
 import math as m
-import utils as ut
+
 import numpy as np
+
+import utils as ut
 
 TASK_NUMBER = 'Task 4'
 FORMULA = 'sin(x) * x'
 STEP_X = 0.001
-TEMPLATE_A = 'enter the beginning of the segment  [a, b], a ='
-TEMPLATE_B = 'enter the end of the segment   [a, b], B ='
+TEMPLATE_A = 'enter the beginning of the segment  [a_begin, b_end], A ='
+TEMPLATE_B = 'enter the end of the segment   [a_begin, b_end], B ='
 
 
 def f_range(start, stop, step):  # range() на пряму не працює з float, то обходимо наступним чином,
@@ -21,19 +23,20 @@ def f_range(start, stop, step):  # range() на пряму не працює з 
         i += step
 
 
-def min_func_expression(a, b):
-    temp_b = a
-    if a > b:
-        b = a
-        a = temp_b
-    return min([m.sin(x) * x for x in np.arange(a, b + STEP_X, STEP_X)])
+def min_func_expression(a_begin, b_end):
+    temp_b = a_begin
+    if a_begin > b_end:
+        b_end = a_begin
+        a_begin = temp_b
+    return min([m.sin(x) * x for x in np.arange(a_begin, b_end + STEP_X, STEP_X)])
 
 
 if __name__ == '__main__':
     ut.print_info(TASK_NUMBER)
     ut.description_task(FORMULA)
 
-    a = ut.inp_variable('a', TEMPLATE_A)
-    b = ut.inp_variable('b', TEMPLATE_B)
+    a = ut.inp_variable('a_begin', TEMPLATE_A)
+    b = ut.inp_variable('b_end', TEMPLATE_B)
 
-    print(f'On the segment [{a}:{b}], function {FORMULA} takes the min value = {min_func_expression(a, b)}')
+    print(f'On the segment [{a}:{b}], function {FORMULA} '
+          f'takes the min value = {min_func_expression(a, b)}')
